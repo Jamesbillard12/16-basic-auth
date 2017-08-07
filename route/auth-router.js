49d8e04b1aceaebf56ec9e379126c9514c376cd9
Jamesbillard12 <jamesbillard12@hotmail.com>
2017-08-07 14:47:25 -0700

    basic auth middleware complete

---
+++ route/auth-router.js
@@ -0,0 +1,4 @@
+'use strict';
+const jsonParser = require('body-parser').json();
+const debug = require('debug')('cfgram:auth-router');
+const Router = require('express')()
--- server.js
+++ server.js
@@ -2,10 +2,17 @@
 
 const express = require('express');
 const debug = require('debug')('cfgram:server');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const cors = require('cors');
+
+const errors = require('./lib/error-middleware.js');
 
 const app = express();
-
 const PORT = process.env.PORT || 3000;
+
+app.use(errors);
 
 app.listen(PORT, () => {
   debug(`server started on ${PORT}`);
