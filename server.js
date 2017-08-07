7962ad5d56d95c22ea19e70b166e3e3b9306b120
Jamesbillard12 <jamesbillard12@hotmail.com>
2017-08-07 15:20:18 -0700

    finished server file

--- server.js
+++ server.js
@@ -7,11 +7,17 @@ const mongoose = require('mongoose');
 const morgan = require('morgan');
 const cors = require('cors');
 
+const authRouter = require('/route/auth-router.js');
 const errors = require('./lib/error-middleware.js');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+mongoose.connect(process.env.MONOGODB_URI);
 
+app.use(cors());
+app.use(morgan('dev'));
+
+app.use(authRouter);
 app.use(errors);
 
 app.listen(PORT, () => {
