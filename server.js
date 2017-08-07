3509b12c3ea8eae9c696bd287d1618faf6c4c334
Jamesbillard12 <jamesbillard12@hotmail.com>
2017-08-07 15:32:16 -0700

    server is still up and running

--- server.js
+++ server.js
@@ -7,12 +7,14 @@ const mongoose = require('mongoose');
 const morgan = require('morgan');
 const cors = require('cors');
 
-const authRouter = require('/route/auth-router.js');
+const authRouter = require('./route/auth-router.js');
 const errors = require('./lib/error-middleware.js');
+
+dotenv.load();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-mongoose.connect(process.env.MONOGODB_URI);
+mongoose.connect(process.env.MONGODB_URI);
 
 app.use(cors());
 app.use(morgan('dev'));
